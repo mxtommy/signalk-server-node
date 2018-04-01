525a5c860794d405f66a3a169ef1d194a6423864
Teppo Kurki <teppo.kurki@iki.fi>
2018-04-01 10:56:54 +0300

    feature: build docker images only for master and tags

--- make-manifest.sh
+++ make-manifest.sh
@@ -1,13 +1,13 @@
 cat >docker-manifest.yml <<EOF
-image: 'signalk/signalk-server:$BRANCH'
+image: 'signalk/signalk-server:$TAG_SPEC'
 manifests:
   -
-    image: 'signalk/signalk-server:linux-amd64-$BRANCH'
+    image: 'signalk/signalk-server:linux-amd64-$TAG_SPEC'
     platform:
       architecture: amd64
       os: linux
   -
-    image: 'signalk/signalk-server:linux-arm32v7-$BRANCH'
+    image: 'signalk/signalk-server:linux-arm32v7-$TAG_SPEC'
     platform:
       architecture: arm
       variant: v7
